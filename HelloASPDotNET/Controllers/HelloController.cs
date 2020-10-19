1dea2a01c9c8ca14018796f075be0efcbdf5424e
cjconnor18 <cjallen1988@gmail.com>
2020-10-19 12:45:51 -0500

    Exercise 10.5 complete

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -15,6 +15,13 @@ namespace HelloASPDotNET.Controllers
         {
             string html = "<form method='post' action='/helloworld/'>" +
                 "<input type='text' name='name' />" + 
+                "<select name='lang'>" +
+                "<option value='english'>English</option>" +
+                "<option value='spanish'>Spanish</option>" + 
+                "<option value='french'>French</option>" +
+                "<option value='thai'>Thai</option>" +
+                "<option value='german'>German</option>" +
+                "</select>" +
                 "<input type='submit' value='greet me!' />" +
                 "</form>";
             return Content(html, "text/html");
@@ -25,9 +32,28 @@ namespace HelloASPDotNET.Controllers
         //[Route("/helloworld/welcome/{name?}")]
         [HttpGet("welcome/{name?}")]
         [HttpPost]
-        public  IActionResult Welcome(string name = "World")
+        public  IActionResult Welcome(string name = "World", string lang = "english")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            return Content("<h1 style='color:red;'>" + CreateMessage(name, lang) + "!</h1>", "text/html");
+        }
+
+        public static string CreateMessage(string name, string lang)
+        {
+            string hello = "Hello";
+            if(lang == "spanish")
+            {
+                hello = "Hola";
+            } else if (lang == "french")
+            {
+                hello = "Bonjour";
+            } else if (lang == "thai")
+            {
+                hello = "Swasdi";
+            } else if (lang == "german")
+            {
+                hello = "Hallo";
+            }
+            return $"{hello} {name}";
         }
     }
 }
