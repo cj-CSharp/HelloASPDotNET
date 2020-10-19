5455f6d78ddc6c28d70e2d54a0cfb04d717e9b11
cjconnor18 <cjallen1988@gmail.com>
2020-10-19 12:03:31 -0500

    10.2 query and path variables

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        //Get: /<controller>/
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+
+        //GET: hello/welcome
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public  IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
