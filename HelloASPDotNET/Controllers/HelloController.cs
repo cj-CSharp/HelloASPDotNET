1af0c5c802c287ec49b21685ac9da3dc29ce4d52
cjconnor18 <cjallen1988@gmail.com>
2020-10-26 14:20:16 -0500

    13.2 Complete

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -13,12 +13,7 @@ namespace HelloASPDotNET.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/hello'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet Me!' />" +
-                "</form>";
-
-            return Content(html, "text/html");
+            return View();
         }
 
         [HttpPost]
